935602eb4c82e1d22f573d0d32264718ae9c8d9f
Jeremy Morales <jeremy.morales@gmail.com>
2020-06-14 12:55:31 -0700

    moving zero done

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,26 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+def shift_zero(arr, l, r):
+    while l < r:
+        arr[l+1], arr[l] = arr[l], arr[l+1]
+        l += 1
+
 def moving_zeroes(arr):
     # Your code here
+    # starting at right side
+    index = end = len(arr) -1
 
-    pass
+    while index >= 0:
+        if arr[index] == 0:
+            shift_zero(arr, index, end)
+            index -= 1
+        else:
+            index -= 1
+    return arr
+
+    
+
 
 
 if __name__ == '__main__':
